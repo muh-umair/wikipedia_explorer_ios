2fc5677f2b459b30e0193aa30b0966f0b575d1e7
Muhammad Umair <muh.umair.bhatti@gmail.com>
2023-10-19 10:59:27 +0200

    Initial Commit

---
+++ WikipediaExplorer/WikipediaExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikipediaExplorerApp.swift
+//  WikipediaExplorer
+//
+//  Created by Muhammad Umair on 19.10.23.
+//
+
+import SwiftUI
+
+@main
+struct WikipediaExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
